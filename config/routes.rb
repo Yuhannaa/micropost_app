de81d3951a5288804d7697d20dc8e490b0e66d12
Yuhannaa <yuhannaa@gmail.com>
2013-04-06 16:10:34 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MicropostApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
